48f72fd5b4fd46ddb0226e800a74f10c75af9833
Feriman <Feriman22@users.noreply.github.com>
2023-03-06 10:28:33 +0100

    Update domain-availability-checker.sh

--- domain-availability-checker.sh
+++ domain-availability-checker.sh
@@ -13,7 +13,7 @@ randomstring2143532.org \
 
 # Check domains one-by-one
 for domain in $DOMAINS; do
-        if whois $domain | egrep -q '^No match|^NOT FOUND|^Not fo|AVAILABLE|^No Data Fou|has not been regi|No entri'; then
+        if whois $domain | egrep -q '^No match|^NOT FOUND|^Not fo|^Domain not found|AVAILABLE|^No Data Fou|has not been regi|No entri'; then
                 echo "$domain is not registered."
         fi
 done
